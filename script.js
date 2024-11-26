03ee639e6fe26c9675a0e1a5aa36b33047b02dd2
jcarmichael9 <526jcarmichael@frhsd.com>
2024-11-26 11:49:15 -0500

    done just need to style a bit more

--- script.js
+++ script.js
@@ -1,54 +1,63 @@
 //Get user name
 let userName = prompt('Hello buddy. what is your name?')
+
+let responseImg = document.getElementById('response-img')
+let responseText = document.getElementById('response-text')
+let greeting = document.getElementById('greeting')
+
 //unput username to website
-userName ? document.getElementById('greeting').innerText=`Hello, ${userName}` : document.getElementById('greeting').innerText='Why did you not tell me your name.'
-
-
+userName ? (greeting.innerText=`Hello, ${userName}`) : (greeting.innerText='Why did you not tell me your name.')
 
 
 function shakeMagic8Ball(){
 
 //Get user question
-let userQuestion = 'Is Hank short?'
-
-userQuestion ? console.log(`${userName} asked, ${userQuestion}.`) : console.log('No question asked.')
+let userQuestion = prompt('What question to want to ask to the most amazing, glorious, respectable, smart, bright, and handsome 8 ball?')
 
 //DO NOT TOUCH THS CODE!
 let randomNumber = Math.floor(Math.random() * 8);
 
 let eightBall = ''
-
+let imageChoice = ''
 switch (randomNumber) {
   case 0:
     eightBall = 'It is certain';
+    imageChoice = 'imgs/certain.jpg'
     break;
 
     case 1:
     eightBall = 'It is decidedly so';
+    imageChoice = 'imgs/decide.jpg'
     break;
 
     case 2:
     eightBall = 'Reply hazy try again';
+    imageChoice = 'imgs/ask.jpg'
     break;
 
     case 3:
     eightBall = 'Cannot predict now';
+    imageChoice = 'imgs/predict.jpg'
     break;
 
     case 4:
     eightBall = 'Do not count on it';
+    imageChoice = 'imgs/count.jpg'
     break;
 
     case 5:
     eightBall = 'My sources say no';
+    imageChoice = 'imgs/sources.png'
     break;
 
     case 6:
     eightBall = 'Outlook not so good';
+    imageChoice = 'imgs/outlook.jpg'
     break;
 
     case 7:
     eightBall = 'Signs point to yes';
+    imageChoice = 'imgs/signs.jpg'
     break;
 
   // Add more cases for 1 to 7
@@ -58,4 +67,9 @@ switch (randomNumber) {
 
 console.log(eightBall)
 
+
+userName ? document.getElementById('response-text').innerText= `${userName} has asked "${userQuestion}" to the most amazing, glorious, respectable, smart, bright, and handsome 8 ball. It's absolutely perfect, never wrong, always correct, never a lie, always a truth answer is, "${eightBall}".` 
+ : document.getElementById('response-text').innerText= `Weirdo who won't tell me their name has asked "${userQuestion}" to the most amazing, glorious, respectable, smart, bright, and handsome 8 ball. It's absolutely perfect, never wrong, always correct, never a lie, always a truth answer is, "${eightBall}."` 
+
+responseImg.src = imageChoice
 }
